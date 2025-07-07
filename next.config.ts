86b0e2810dc038f5e18565f25784d0ed2a5f71d2
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-07 18:17:44 +0300

    feat: added permitted hostnames
    - images.pexels.com tmp hostname for client side slider( will be moved to my cdn)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mwy047us3b.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
