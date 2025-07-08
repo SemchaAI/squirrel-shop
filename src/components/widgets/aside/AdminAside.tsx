c9980a060cb891e4c5f6bd8ca05923e9d565c59d
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 11:13:00 +0300

    !feat(tmp): admin aside

---
+++ src/components/widgets/aside/AdminAside.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "@/components/features";
+// import Link from "next/link";
+
+// interface IProps {}
+
+export const AdminAside = () => {
+  return (
+    <aside className="flex w-80 flex-col gap-5 border-r border-border bg-ui py-4 sm:px-2 md:px-4 lg:px-6 xl:px-8 2xl:px-10">
+      {/* <Link href="/admin">Home</Link> */}
+      <NavLink href="/admin">Products (Home)</NavLink>
+      <NavLink href="/admin/products">Product Variants</NavLink>
+      <NavLink href="/admin/orders">Orders</NavLink>
+    </aside>
+  );
+};
