6e27c30e7b38d9c2ceafe97bb944594ca5e9b1f7
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 14:40:08 +0300

    feat(product): add ProductControls with add-to-cart, stock display, favorite toggle, and checkout link

---
+++ src/components/entities/product/ProductControls.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+
+import { AddToCart, ToggleFavoriteButton } from "@/components/features";
+import { StockDescription } from "./StockDescription";
+import { ROUTES } from "@/utils/config";
+import { BarcodeIcon } from "lucide-react";
+import { useCartStore } from "@/utils/hooks";
+import { Tooltip } from "@/components/shared";
+
+interface IProps {
+  stock: number;
+  id: string;
+}
+export const ProductControls = ({ id, stock }: IProps) => {
+  const items = useCartStore((state) => state.items);
+  console.log("id", id, items);
+  const inCart = items.some((item) => item.productVariantId === id);
+  return (
+    <div className="flex justify-between gap-4">
+      {/* <QuantitySelector quantity={currQuantity} setQuantity={quantityHandler} /> */}
+      <div className="flex w-full flex-wrap justify-between gap-2">
+        <div className="flex flex-wrap gap-2">
+          <AddToCart id={id} stock={stock} />
+          <StockDescription inStock={stock} />
+        </div>
+        <div className="flex flex-wrap gap-2">
+          {inCart ? (
+            <Link
+              href={ROUTES.CART}
+              className="flex items-center gap-2 rounded-full border border-primary px-4 py-2 text-primary transition-colors hover:bg-primary hover:text-white"
+            >
+              <BarcodeIcon size={20} /> Go to cart
+            </Link>
+          ) : (
+            <Tooltip content="Add to cart firstly">
+              <div className="flex cursor-not-allowed items-center gap-2 rounded-full border border-primary px-4 py-2 text-primary transition-colors hover:bg-primary hover:text-white">
+                <BarcodeIcon size={20} /> Checkout
+              </div>
+            </Tooltip>
+          )}
+
+          <ToggleFavoriteButton id={id} />
+        </div>
+      </div>
+    </div>
+  );
+};
