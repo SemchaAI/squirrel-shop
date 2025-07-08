3cc5a9dc9826d4723dbc906987df27aaeee4de99
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 14:56:57 +0300

    feat: add memoized product card with image, title, price, SKU, cart and favorite buttons

---
+++ src/components/entities/cards/ProductCard.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { AddToCart, ToggleFavoriteButton } from "@/components/features";
+
+import { memo } from "react";
+import type { ProductVariants } from "@prisma/client";
+import { ROUTES } from "@/utils/config";
+
+interface IProps {
+  product: ProductVariants;
+}
+
+const ProductCardComponent = ({ product }: IProps) => {
+  const url = product.previewImage
+    ? process.env.NEXT_PUBLIC_IMAGE_CDN_URL + product.previewImage
+    : "/static/images/no-image360.webp";
+  return (
+    <li key={product.id} className="flex flex-col gap-4 rounded-md bg-ui p-2">
+      <Link
+        className="relative flex w-full justify-center"
+        href={`${ROUTES.PRODUCT}/${product.slug}`}
+      >
+        <Image
+          className="rounded-md"
+          src={url}
+          alt={product.title}
+          width={280}
+          height={280}
+        />
+      </Link>
+      <div className="flex justify-between gap-1 font-bold">
+        <p>{product.title}</p>
+        <p>{product.price} MDL</p>
+      </div>
+      <p className="text-sm text-gray-500">{product.sku}</p>
+      <div className="mt-auto flex items-center justify-between">
+        <AddToCart id={product.id} stock={product.stock} />
+        <ToggleFavoriteButton id={product.id} />
+      </div>
+    </li>
+  );
+};
+
+export const ProductCard = memo(ProductCardComponent);
+ProductCard.displayName = "ProductCard";
