b16e6b1f9e482d60e3b44dd05d2320ab02369d49
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 14:14:24 +0300

    feat(cart): add shopping bag control with mini cart and counter badge

---
+++ src/components/features/cart/ShoppingBagControl.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { ShoppingBagIcon } from "lucide-react";
+
+import { MiniCart } from "@/components/widgets";
+import { CountBadge } from "@/components/shared";
+import { useCartStore, useClickOutside } from "@/utils/hooks";
+import { usePathname } from "next/navigation";
+
+export const ShoppingBagControl = () => {
+  const ref = useRef(null);
+  const [open, setOpen] = useState(false);
+  const path = usePathname();
+  const counter = useCartStore((state) => state.counter);
+
+  useClickOutside(ref, () => {
+    if (open) setOpen(false);
+  });
+  useEffect(() => {
+    if (open) setOpen(false);
+  }, [path]);
+
+  return (
+    <div ref={ref}>
+      <div className="relative">
+        <button
+          className="flex h-8 w-8 cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <ShoppingBagIcon className="cursor-pointer" size={32} />
+        </button>
+        {counter > 0 && <CountBadge key={counter} totalItems={counter} />}
+      </div>
+
+      <MiniCart isOpen={open} />
+    </div>
+  );
+};
