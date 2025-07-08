90e7e08740898b07e2c82edc72342031bf77c377
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 15:31:40 +0300

    feat: add admin layout with header and sidebar for admin pages

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,25 @@
+import { AdminAside, AdminHeader } from "@/components/widgets";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Squirrel Shop Admin",
+  description: "Online store with nextjs and prisma",
+};
+
+export default async function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <AdminHeader />
+      <div className="flex flex-1 gap-5">
+        <AdminAside />
+        <main className="flex flex-1 flex-col pr-1 sm:pr-2 md:pr-4 lg:pr-6 xl:pr-8 2xl:pr-10">
+          {children}
+        </main>
+      </div>
+    </>
+  );
+}
