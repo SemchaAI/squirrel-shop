c593210b21586d44be9244c5cfca044d87950e71
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 11:57:25 +0300

    feat: temporal menu for logout user

---
+++ src/components/widgets/user/UserMenu.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { Container } from "@/components/shared";
+import { useAnimatedPresence } from "@/utils/hooks";
+import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { signOut } from "next-auth/react";
+
+interface IProps {
+  isOpen: boolean;
+}
+
+export const UserMenu = ({ isOpen }: IProps) => {
+  const shouldRender = useAnimatedPresence(isOpen, 300);
+
+  return (
+    <Container
+      className={`absolute top-14 right-0 z-10 w-70 transition-all duration-300 ${isOpen ? "animate-fade-in-down" : "animate-fade-out-up"} ${shouldRender ? "" : "hidden"}`}
+    >
+      <div className="flex justify-between">
+        <SettingsIcon />
+        <LogOutIcon
+          className="cursor-pointer"
+          onClick={() => signOut({ redirect: true, redirectTo: "/" })}
+        />
+      </div>
+    </Container>
+  );
+};
