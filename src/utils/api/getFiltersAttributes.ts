b15ac3842ba1d04c6ea276b76bc8f75c11ae0035
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 10:39:38 +0300

    feat(filters): add cached query for category-based attribute filtering
    - Uses `unstable_cache` to cache attributes for 5 minutes
    - Fetches variationName, variationOptionValue, and hexCode by category slug

---
+++ src/utils/api/getFiltersAttributes.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import prisma from "@/prismaClient";
+import type { IAttributeInput } from "@/models/filters";
+import { unstable_cache } from "next/cache";
+
+export const getFiltersAttributes = unstable_cache(
+  async (slug: string): Promise<IAttributeInput[]> => {
+    console.log("getFiltersAttributes");
+    const attributesInCategory = await prisma.productVariantsOptions.findMany({
+      select: {
+        variationOptionValue: true,
+        variationName: true,
+        variationOption: {
+          select: {
+            hexCode: true,
+          },
+        },
+      },
+      where: {
+        productVariant: {
+          product: {
+            categories: {
+              some: {
+                slug: slug,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return attributesInCategory;
+  },
+  ["slug"],
+  { revalidate: 300 },
+);
