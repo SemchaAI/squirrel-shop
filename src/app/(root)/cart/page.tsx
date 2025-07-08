393fcd24aa122d13fda739725d41f0bcde7cc260
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 15:33:16 +0300

    feat: add cart page with loading state, empty state, and cart items list

---
+++ src/app/(root)/cart/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useShallow } from "zustand/shallow";
+import { Squirrel } from "lucide-react";
+
+import { CartCard, EmptyCart, OrderSummary } from "@/components/entities";
+import { useCartStore } from "@/utils/hooks";
+import { ClearCart } from "@/components/features";
+
+export default function CartPage() {
+  const { items, isLoading } = useCartStore(
+    useShallow((state) => {
+      return {
+        items: state.items,
+        isLoading: state.isLoading,
+      };
+    }),
+  );
+
+  if (isLoading && items.length === 0)
+    return (
+      <div className="mt-2 flex grow items-center justify-center gap-2 p-2">
+        <Squirrel className="animate-pulse" size={80} />
+        <div className="animate-pulse text-2xl">Loading...</div>
+      </div>
+    );
+  if (items.length === 0 && !isLoading) return <EmptyCart />;
+
+  return (
+    <section className="wrapper mt-5 py-5">
+      <div className="flex flex-col gap-5 lg:flex-row">
+        <div className="flex-1">
+          <div className="mb-6 flex justify-between gap-2">
+            <h2 className="text-4xl font-bold">Cart</h2>
+            <ClearCart />
+          </div>
+
+          <ul className="w-full">
+            {items.map((item) => (
+              <CartCard key={item.id} item={item} />
+            ))}
+          </ul>
+        </div>
+
+        <OrderSummary items={items} />
+      </div>
+    </section>
+  );
+}
