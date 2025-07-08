87f8f2c632ceaa7ec222b723b8e472d8d72d7367
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-08 15:32:19 +0300

    feat: add admin product page to edit product variant details

---
+++ src/app/(admin)/admin/product/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import prisma from "@/prismaClient";
+
+import { ProductVariantForm } from "@/components/entities";
+
+interface IProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function AdminProductPage({ params }: IProps) {
+  const { slug } = await params;
+
+  const [product, categories, variations] = await prisma.$transaction([
+    prisma.productVariants.findUnique({
+      where: { slug },
+      include: {
+        product: {
+          include: {
+            categories: true,
+          },
+        },
+        images: true,
+        options: true,
+      },
+    }),
+    prisma.category.findMany(),
+    prisma.variations.findMany({
+      include: {
+        options: true,
+      },
+    }),
+  ]);
+  console.log("variations");
+
+  if (!product) return <div>Product not found</div>;
+  return (
+    <ProductVariantForm
+      product={product}
+      categories={categories}
+      variations={variations}
+      slug={slug}
+    />
+  );
+}
