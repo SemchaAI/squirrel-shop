bbd63c60708064f76a5c5b57286da4d842d51f38
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-09 14:58:10 +0300

    feat(filter): add SearchFilter with debounce and URL query syncing

---
+++ src/components/features/filters/SearchFilter.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
+
+import { SearchInput } from "@/components/features";
+import { useDebounce } from "@/utils/hooks";
+
+import { buildUrlQuery } from "@/utils/helpers/buildUrlQuery";
+
+export const SearchFilter = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("q") || "";
+
+  const [query, setQuery] = useState(search);
+  const debouncedQuery = useDebounce(query, 700);
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const RefreshClick = () => {
+    setQuery("");
+  };
+
+  useEffect(() => {
+    if (debouncedQuery === search) return;
+
+    const url = buildUrlQuery(pathname, searchParams, {
+      q: debouncedQuery ? debouncedQuery : undefined,
+    });
+
+    router.push(url, { scroll: false });
+  }, [debouncedQuery, pathname, searchParams, router, search]);
+
+  return (
+    <SearchInput
+      query={query || ""}
+      onChange={setQuery}
+      onClear={RefreshClick}
+    />
+  );
+};
