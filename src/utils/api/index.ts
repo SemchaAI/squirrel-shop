68667ef6aab42a3ddc1bcdbb3caf0d701dc2d62b
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-09 16:07:38 +0300

    chore: reexport

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -4,6 +4,6 @@ export { getFiltersAttributes } from "./getFiltersAttributes";
 
 //http
 export { queryKeys } from "./queryKeys";
-export { fetchProduct } from "./http/product";
+export { fetchProduct, searchProducts } from "./http/product";
 export { fetchMedia } from "./http/media";
 export { createCheckoutSession } from "./http/order";
