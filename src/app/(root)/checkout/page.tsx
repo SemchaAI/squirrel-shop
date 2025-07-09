199e58cebc9311bfe6752c8282985109a04df4b6
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-09 12:10:00 +0300

    feat: add ssr user data, for defaultValues in form

--- src/app/(root)/checkout/page.tsx
+++ src/app/(root)/checkout/page.tsx
@@ -7,29 +7,37 @@ import prisma from "@/prismaClient";
 export default async function CheckoutPage() {
   const session = await auth();
   if (!session) return notFound();
-  const items = await prisma.cartProduct.findMany({
-    where: {
-      cart: {
-        userId: session.user.id,
-      },
-    },
-    include: {
-      productVariant: {
-        select: {
-          id: true,
-          title: true,
-          price: true,
-          previewImage: true,
-          previousPrice: true,
-          sku: true,
+  const [items, user] = await prisma.$transaction([
+    prisma.cartProduct.findMany({
+      where: {
+        cart: {
+          userId: session.user.id,
         },
       },
-    },
-  });
+      include: {
+        productVariant: {
+          select: {
+            id: true,
+            title: true,
+            price: true,
+            previewImage: true,
+            previousPrice: true,
+            sku: true,
+          },
+        },
+      },
+    }),
+    prisma.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+    }),
+  ]);
+  if (!user) return notFound();
   if (!items.length) return <EmptyCart title="Checkout" />;
   return (
     <section className="wrapper mt-5 pb-5">
-      <OrderForm items={items} userId={session.user.id} />
+      <OrderForm items={items} user={user} />
     </section>
   );
 }
