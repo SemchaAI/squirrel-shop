330217d3c27c0081205aec9b81303f72067ddd30
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-09 13:02:04 +0300

    feat(api): add createCheckoutSession mutation for initiating Stripe checkout

---
+++ src/utils/api/http/order.ts
@@ -0,0 +1,19 @@
+// api/utils/mutations.ts
+import { IOrder } from "@/models/orders";
+import { IDataResponse } from "@/models/response";
+import { API_ROUTES } from "@/utils/config";
+
+export const createCheckoutSession = async (
+  payload: IOrder,
+): Promise<IDataResponse<{ url: string | null }>> => {
+  const res = await fetch(`${API_ROUTES.CHECKOUT}`, {
+    method: "POST",
+    body: JSON.stringify({ payload }),
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error?.message || "Checkout failed");
+  }
+  return res.json();
+};
