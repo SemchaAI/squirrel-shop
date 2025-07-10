a38bc3350e0237b170437d42ea5ec0792dfe9cfc
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-10 10:29:53 +0300

    feat: text area rhf field for forms

---
+++ src/components/features/fields/TextAreaField.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useFormContext } from "react-hook-form";
+
+interface IProps {
+  id: string;
+  className?: string;
+  placeholder?: string;
+}
+
+export const TextAreaField = ({
+  id,
+  placeholder,
+  className,
+  ...props
+}: IProps) => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useFormContext();
+
+  const value = watch(id);
+
+  return (
+    <div className="relative w-full">
+      <textarea
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        {...register(id)}
+        {...props}
+        className={`min-h-[100px] w-full resize-none rounded-lg border border-border bg-app p-3 text-text-high outline-none focus:border-primary ${className || ""} `}
+      />
+      {errors[id] && (
+        <p className="mt-1 text-sm text-error">
+          {errors[id]?.message as string}
+        </p>
+      )}
+    </div>
+  );
+};
+
+TextAreaField.displayName = "TextAreaField";
