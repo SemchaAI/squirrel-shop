8cd3167cbaae54889658a2ea4c8a159bbec91861
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-16 10:44:42 +0300

    feat(auth): add reset password form with token verification

---
+++ src/components/entities/forms/ResetPasswordForm.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { AsteriskIcon, KeyRoundIcon } from "lucide-react";
+
+import { Form } from "./Form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InputField } from "@/components/features";
+import { Button } from "@/components/shared";
+import { resetPasswordWithToken } from "@/actions/auth";
+import { ResetPasswordSchema, ROUTES } from "@/utils/config";
+
+import type { TResetPasswordSchema } from "@/utils/config";
+
+export const ResetPasswordForm = () => {
+  const router = useRouter();
+
+  const params = useSearchParams();
+  const token = params.get("token");
+
+  const form = useForm<TResetPasswordSchema>({
+    resolver: zodResolver(ResetPasswordSchema),
+  });
+
+  const submitHandler = async (data: TResetPasswordSchema) => {
+    if (!token) {
+      toast.error("Invalid reset link.");
+      return;
+    }
+    console.log("payload", data);
+    const res = await resetPasswordWithToken(token, data);
+
+    if (res.isSuccess) {
+      toast.success("Password reset successfully!");
+      router.push(ROUTES.SIGNIN);
+    } else {
+      toast.error(res.message);
+    }
+  };
+
+  return (
+    <Form form={form} onSubmit={submitHandler} title="Set New Password">
+      <InputField
+        id="newPassword"
+        label="New Password"
+        type="password"
+        Icon={KeyRoundIcon}
+        placeholder="Enter new password"
+        EyeIcon
+      />
+      <InputField
+        id="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        Icon={AsteriskIcon}
+        placeholder="Confirm new password"
+      />
+
+      <Button type="submit" className="w-full">
+        Reset Password
+      </Button>
+    </Form>
+  );
+};
