df222848a74c30d6e8150c678f0e9770eef7dd6d
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-16 14:23:13 +0300

    fix: changed key (isnt public now)

--- src/utils/stripe-client.ts
+++ src/utils/stripe-client.ts
@@ -2,10 +2,8 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const getStripe = () => {
-  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-    throw new Error(
-      "Missing NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY in environment variables",
-    );
+  if (!process.env.STRIPE_PUBLISHABLE_KEY) {
+    throw new Error("Missing STRIPE_PUBLISHABLE_KEY in environment variables");
   }
-  return loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+  return loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
 };
