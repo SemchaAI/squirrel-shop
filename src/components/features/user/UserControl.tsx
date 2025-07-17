a4ecb92c5722720049233328b4395af568c8ca1e
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-17 17:32:45 +0300

    refactor: simplify UserControl to link avatar to profile
    Refactored UserControl to remove dropdown menu logic and instead link the user avatar directly to the profile page. Cleaned up unused state, refs, and imports for a simpler, more direct user experience.

--- src/components/features/user/UserControl.tsx
+++ src/components/features/user/UserControl.tsx
@@ -1,54 +1,35 @@
 "use client";
-import { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { UserIcon } from "lucide-react";
 
-import { useClickOutside } from "@/utils/hooks";
-import { UserMenu } from "@/components/widgets";
 import { ROUTES } from "@/utils/config";
-import { useState } from "react";
 
 export const UserControl = () => {
-  const ref = useRef(null);
-  const [open, setOpen] = useState(false);
   const session = useSession();
-
-  useClickOutside(ref, () => {
-    if (open) setOpen(false);
-  });
-
   const avatar = session.data?.user?.avatar;
   const avatarUrl =
     avatar && `${process.env.NEXT_PUBLIC_IMAGE_CDN_URL}${avatar}`;
+  const user = session.data?.user;
 
   return (
-    <div ref={ref}>
-      {/* <div className="relative"> */}
-      {session.data?.user ? (
-        <>
-          <button
-            className="flex h-8 w-8 cursor-pointer"
-            type="button"
-            onClick={() => setOpen((prev) => !prev)}
-          >
-            <Image
-              src={avatarUrl || "/static/images/avatar.png"}
-              alt="User Avatar"
-              width={32}
-              height={32}
-              className="rounded-full"
-            />
-          </button>
-          <UserMenu isOpen={open} />
-        </>
+    <>
+      {user ? (
+        <Link href={`${ROUTES.PROFILE}/${user.id}`}>
+          <Image
+            src={avatarUrl || "/static/images/avatar.png"}
+            alt="User Avatar"
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        </Link>
       ) : (
         <Link href={ROUTES.SIGNIN}>
           <UserIcon className="cursor-pointer" size={32} />
         </Link>
       )}
-      {/* </div> */}
-    </div>
+    </>
   );
 };
