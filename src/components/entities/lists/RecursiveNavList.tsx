f5468b231827b48f9dcc46ee221642b44f02ebd5
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-17 17:16:42 +0300

    feat(nav): add recursive navigation list with expandable submenus

---
+++ src/components/entities/lists/RecursiveNavList.tsx
@@ -0,0 +1,55 @@
+import { NavLink } from "@/components/features";
+import { ExpandableList } from "./ExpandableList";
+
+import type { TNavRoute } from "@/models/routes";
+
+interface RecursiveNavListProps {
+  routes: TNavRoute[];
+  depth?: number;
+}
+
+export const RecursiveNavList: React.FC<RecursiveNavListProps> = ({
+  routes,
+  depth = 0,
+}) => {
+  return (
+    <ul
+      className={`flex w-full flex-1 flex-col gap-4 py-2 text-xl ${depth > 0 ? "pl-2" : ""}`}
+    >
+      {routes.map((route, index) => {
+        const key = `${route.name}-${index}`;
+        if (route.children && route.children.length > 0) {
+          return (
+            <ExpandableList
+              className="mb-0 border-border px-2 pb-4"
+              title={
+                <NavLink
+                  key={key}
+                  href={route.href}
+                  className="flex w-full justify-start gap-2"
+                >
+                  {route.Icon && <route.Icon />}
+                  {route.name}
+                </NavLink>
+              }
+              key={key}
+            >
+              <RecursiveNavList routes={route.children} depth={depth + 1} />
+            </ExpandableList>
+          );
+        }
+
+        return (
+          <NavLink
+            key={key}
+            href={route.href}
+            className="flex w-full justify-start gap-2 border-b border-border p-2"
+          >
+            {route.Icon && <route.Icon />}
+            {route.name}
+          </NavLink>
+        );
+      })}
+    </ul>
+  );
+};
