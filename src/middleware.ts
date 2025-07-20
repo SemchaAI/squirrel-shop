c0896b397284b3d31725cc91224f871fd710e79e
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-20 20:33:34 +0300

    refactor(auth): replace auth wrapper with simplest getToken for session data(~146kb to 44.6kb)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,46 @@
-// export { auth as middleware } from "@/auth";
-import { NextRequest, NextResponse } from "next/server";
-// import NextAuth from "next-auth";
-import { auth } from "./auth";
-import { ProtectedRoutes, ROUTES } from "./utils/config/routes";
+import { type NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export default auth(async function middleware(req: NextRequest) {
-  const session = await auth();
+import { ProtectedRoutes } from "@/utils/config/routes/protected";
+import { ROUTES } from "@/utils/config/routes/routes";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const { pathname } = req.nextUrl;
-  console.log("middleware", session?.user.role, pathname);
-  // Your custom middleware logic goes here
-  for (const route of ProtectedRoutes) {
-    if (pathname.startsWith(route.href)) {
-      if (!session) {
-        return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
-      }
+  console.log("middleware", token?.role, pathname);
 
-      const userRole = session.user.role;
-      if (!route.access.includes(userRole)) {
-        return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
-      }
-    }
+  if (
+    !token &&
+    ProtectedRoutes.some((route) => pathname.startsWith(route.href))
+  ) {
+    return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
   }
-});
+
+  // Check role
+  const route = ProtectedRoutes.find((route) =>
+    pathname.startsWith(route.href),
+  );
+  if (route && !route.access.includes(token?.role || "GUEST")) {
+    return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
+  }
+
+  return NextResponse.next();
+}
+
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|static/).*)"],
 };
+
+// Your custom middleware logic goes here
+// for (const route of ProtectedRoutes) {
+//   if (pathname.startsWith(route.href)) {
+//     if (!session) {
+//       return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
+//     }
+
+//     const userRole = session.user.role;
+//     if (!route.access.includes(userRole)) {
+//       return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
+//     }
+//   }
+// }
