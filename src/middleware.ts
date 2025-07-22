8291b1af1a8120af1d554b47996c655310701c6a
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-22 13:39:11 +0300

    feat: add another matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,19 +28,11 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|static/).*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico|static/).*)"],
 };
-
-// Your custom middleware logic goes here
-// for (const route of ProtectedRoutes) {
-//   if (pathname.startsWith(route.href)) {
-//     if (!session) {
-//       return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
-//     }
-
-//     const userRole = session.user.role;
-//     if (!route.access.includes(userRole)) {
-//       return NextResponse.redirect(new URL(ROUTES.SIGNIN, req.url));
-//     }
-//   }
-// }
