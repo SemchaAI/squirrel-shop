a6eb35e4c2360645000c7a4080d81b4cb9d3023f
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-23 11:37:18 +0300

    refactor(ui): better size for pages with forms, also add pagination with global class wrapper

--- src/app/(root)/signin/page.tsx
+++ src/app/(root)/signin/page.tsx
@@ -2,8 +2,8 @@ import { SignInForm } from "@/components/entities/forms/SigInForm";
 
 export default function SignInPage() {
   return (
-    <section className="flex h-[calc(100dvh-80px)] w-full flex-col">
-      <div className="m-auto w-full max-w-1/3 min-w-80">
+    <section className="wrapper flex h-[calc(100dvh-80px)] w-full flex-col">
+      <div className="m-auto w-full max-w-120 min-w-80">
         <SignInForm />
       </div>
     </section>
--- src/app/(root)/signup/page.tsx
+++ src/app/(root)/signup/page.tsx
@@ -2,8 +2,8 @@ import { SignUpForm } from "@/components/entities/forms/SignUpForm";
 
 export default function SignUpPage() {
   return (
-    <section className="flex h-[calc(100dvh-80px)] w-full flex-col">
-      <div className="m-auto w-full max-w-1/3 min-w-80">
+    <section className="wrapper flex h-[calc(100dvh-80px)] w-full flex-col">
+      <div className="m-auto w-full max-w-120 min-w-80">
         <SignUpForm />
       </div>
     </section>
