7799c0baed0df50f9e6d95512672186cde123c68
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-26 12:11:25 +0300

    eat(form): add StarRatingField  component integrated with RHF

---
+++ src/components/features/fields/StarRatingField.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useController, useFormContext } from "react-hook-form";
+import { StarRating } from "@/components/entities/product/StarRating";
+import { useState } from "react";
+
+interface StarRatingFieldProps {
+  label: string;
+  name: string;
+  size?: number;
+  color?: string;
+}
+
+export const StarRatingField: React.FC<StarRatingFieldProps> = ({
+  label,
+  name,
+  size,
+  color,
+}) => {
+  const { control } = useFormContext();
+  const {
+    field: { value, onChange },
+    fieldState: { error },
+  } = useController({
+    name,
+    control,
+    defaultValue: 0,
+  });
+
+  const [hoveredIndex, setHoveredIndex] = useState(0);
+
+  const errorMessage = error?.message;
+  return (
+    <div className="flex flex-col gap-px">
+      <p className="pl-2 text-xs text-text-high" id={name}>
+        {label}:
+      </p>
+      <StarRating
+        id={name}
+        rating={value}
+        onRate={onChange}
+        size={size}
+        color={color}
+        className="cursor-pointer px-2 py-1"
+        hoveredIndex={hoveredIndex}
+        setHoveredIndex={setHoveredIndex}
+        aria-labelledby={name}
+      />
+      <div className="flex h-6 items-center border-t border-border pl-2 text-xs text-error">
+        {errorMessage}
+      </div>
+    </div>
+  );
+};
