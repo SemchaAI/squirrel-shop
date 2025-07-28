5adde4cd3cec6002840ab2ecbc4aa3e72bd19fed
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-28 12:38:52 +0300

    feat(review): add useReview hook with infinite scroll support
    -Implements an infinite query for product reviews using initial data and pagination.
    -Supports lazy loading with optional enable flag.

---
+++ src/utils/hooks/query/useReview.ts
@@ -0,0 +1,29 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { queryKeys } from "@/utils/api/queryKeys";
+import { getReviews } from "@/utils/api/http/review";
+
+import type { IProductReview } from "@/models/review";
+
+export const useReview = (
+  productId: string,
+  initialReviews: IProductReview[],
+  enabled: boolean,
+) => {
+  return useInfiniteQuery({
+    queryKey: queryKeys.review(productId),
+    queryFn: async ({ pageParam = 1 }) =>
+      getReviews({ productId, page: pageParam }),
+    getNextPageParam: (lastPage, allPages) => {
+      const hasMore = lastPage.length > 0;
+      return hasMore ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1,
+    initialData: {
+      pageParams: [1],
+      pages: [initialReviews],
+    },
+
+    enabled: !!productId && enabled,
+  });
+};
