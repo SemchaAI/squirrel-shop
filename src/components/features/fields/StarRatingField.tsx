106e51531636eabff311522be55f5bacbcd9eb24
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-28 15:11:55 +0300

    refactor: returned possibility to change color of stars

--- src/components/features/fields/StarRatingField.tsx
+++ src/components/features/fields/StarRatingField.tsx
@@ -7,12 +7,14 @@ interface StarRatingFieldProps {
   label: string;
   name: string;
   size?: number;
+  color?: string;
 }
 
 export const StarRatingField: React.FC<StarRatingFieldProps> = ({
   label,
   name,
   size,
+  color,
 }) => {
   const { control } = useFormContext();
   const {
@@ -37,6 +39,7 @@ export const StarRatingField: React.FC<StarRatingFieldProps> = ({
         rating={value}
         onRate={onChange}
         size={size}
+        color={color}
         className="cursor-pointer px-2 py-1"
         hoveredIndex={hoveredIndex}
         setHoveredIndex={setHoveredIndex}
