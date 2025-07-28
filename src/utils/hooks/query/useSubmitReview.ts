8c3be7deddd018d0e889af0e1c9923bb48fcafa3
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-28 12:38:11 +0300

    feat(review): add useSubmitReview hook with mutation and toast notifications
    -Implements a custom React Query mutation hook to submit product reviews.
    -Handles success with query invalidation and error with toast feedback.

---
+++ src/utils/hooks/query/useSubmitReview.ts
@@ -0,0 +1,23 @@
+import { queryKeys } from "@/utils/api";
+import { createReview } from "@/utils/api/http/review";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export const useSubmitReview = (productId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: Parameters<typeof createReview>[1]) =>
+      createReview(productId, data),
+    onSuccess: (data) => {
+      toast.success(data.message);
+      queryClient.invalidateQueries({ queryKey: queryKeys.review(productId) });
+    },
+    onError: (errorMessage: string) => {
+      console.log("[useSubmitReview]", errorMessage);
+      toast.error(errorMessage || "Something went wrong! Try later.", {
+        duration: 5000,
+      });
+    },
+  });
+};
