a1acfb75a7defc05d308ff78fa51922337a02773
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-28 15:14:52 +0300

    refactor(ui): added sale badge and review stats on product card

--- src/components/entities/cards/ProductCard.tsx
+++ src/components/entities/cards/ProductCard.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { memo } from "react";
 
+import { StarRating } from "@/components/entities/product/StarRating";
 import { AddToCart } from "@/components/features/cart/AddToCart";
 import { ToggleFavoriteButton } from "@/components/features/favorite/ToggleFavoriteButton";
 import { ROUTES } from "@/utils/config/routes/routes";
-
-import type { ProductVariants } from "@prisma/client";
+import type { IProductCard } from "@/models/product";
+import { SaleBadge } from "../product/SaleBadge";
 
 interface IProps {
-  product: ProductVariants;
+  product: IProductCard;
   priority?: boolean;
 }
 
@@ -18,7 +19,7 @@ const ProductCardComponent = ({ product, priority = false }: IProps) => {
     ? process.env.NEXT_PUBLIC_IMAGE_CDN_URL + product.previewImage
     : "/static/images/no-image360.webp";
   return (
-    <li key={product.id} className="flex flex-col gap-4 rounded-md bg-ui p-2">
+    <li key={product.id} className="flex flex-col gap-3 rounded-md bg-ui p-2">
       <Link
         className="relative flex w-full justify-center"
         href={`${ROUTES.PRODUCT}/${product.slug}`}
@@ -32,11 +33,36 @@ const ProductCardComponent = ({ product, priority = false }: IProps) => {
           height={280}
         />
       </Link>
-      <div className="flex justify-between gap-1 font-bold">
-        <p>{product.title}</p>
-        <p>{product.price} MDL</p>
+
+      <div className="flex flex-col justify-between gap-4 font-bold">
+        <div>
+          <p>{product.title}</p>
+          <p className="text-sm text-text-low">{product.sku}</p>
+        </div>
+
+        <div className="flex flex-wrap items-center gap-1">
+          <StarRating rating={product.product.averageRating} />
+          <span className="font-inter text-sm">
+            ({product.product.reviewCount})
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          {product.previousPrice && (
+            <div className="flex items-center gap-2">
+              <SaleBadge
+                price={product.price}
+                previousPrice={product.previousPrice}
+              />
+              <p className="text-text-low line-through">
+                {product.previousPrice} MDL
+              </p>
+            </div>
+          )}
+          <p>{product.price} MDL</p>
+        </div>
       </div>
-      <p className="text-sm text-text-low">{product.sku}</p>
+
       <div className="mt-auto flex items-center justify-between">
         <AddToCart id={product.id} stock={product.stock} />
         <ToggleFavoriteButton id={product.id} title={product.title} />
