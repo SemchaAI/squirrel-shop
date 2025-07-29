8af15d7748de347230b10282596f1d55184394c4
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-07-29 18:23:33 +0300

    feat: add cron job for auto remove old rejected comments

--- src/app/api/cron/user/route.ts
+++ src/app/api/cron/user/route.ts
@@ -13,6 +13,7 @@ export async function GET(request: NextRequest) {
 
   const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
   const maxAgeAgo = new Date(Date.now() - MAX_AGE * 1000);
+  const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
   const deleted = await prisma.user.deleteMany({
     where: {
@@ -30,5 +31,14 @@ export async function GET(request: NextRequest) {
     },
   });
 
-  return NextResponse.json({ message: `✅ Deleted ${deleted.count} users.` });
+  const deletedComms = await prisma.productReview.deleteMany({
+    where: {
+      createdAt: { lt: oneDayAgo },
+      status: "REJECTED",
+    },
+  });
+
+  return NextResponse.json({
+    message: `✅ Deleted ${deleted.count} users. Deleted ${deletedComms.count} comments`,
+  });
 }
