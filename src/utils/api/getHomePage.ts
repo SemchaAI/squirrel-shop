67fbec44ee8739fab7b0e2ca9f96ee0feba098f7
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-08-02 16:37:42 +0300

    refactor: moved to helper all page server side data fetching with unstable cache

---
+++ src/utils/api/getHomePage.ts
@@ -0,0 +1,37 @@
+import { unstable_cache } from "next/cache";
+import prisma from "@/prismaClient";
+
+export const getHomePage = unstable_cache(
+  async () => {
+    const [featured, newArrivals, categories] = await prisma.$transaction([
+      prisma.productVariants.findMany({
+        take: 5,
+        where: {
+          product: {
+            categories: {
+              some: {
+                slug: "featured",
+              },
+            },
+          },
+        },
+        include: {
+          product: { select: { reviewCount: true, averageRating: true } },
+        },
+      }),
+      prisma.productVariants.findMany({
+        take: 5,
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          product: { select: { reviewCount: true, averageRating: true } },
+        },
+      }),
+      prisma.category.findMany(),
+    ]);
+    return { featured, newArrivals, categories };
+  },
+  ["homePage"],
+  { revalidate: 300 },
+);
