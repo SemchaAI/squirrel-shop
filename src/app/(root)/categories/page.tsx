a9b67ccab6c4a7ce2893dc14e5e8e094d29bd8d8
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-10 11:25:11 +0300

    fix: updated image url

--- src/app/(root)/categories/page.tsx
+++ src/app/(root)/categories/page.tsx
@@ -13,27 +13,30 @@ export default async function CategoriesPage() {
     <section className="wrapper flex flex-1 flex-col gap-2 py-4">
       <h1 className="py-2 text-3xl font-bold">All Categories</h1>
       <ul className="grid [grid-template-columns:repeat(auto-fill,minmax(280px,1fr))] gap-x-2 gap-y-4 sm:gap-x-6 sm:gap-y-10 xl:gap-10">
-        {categories.map((category) => (
-          <li key={category.id}>
-            <Link
-              href={`${ROUTES.CATEGORY}/${category.slug}`}
-              key={category.id}
-            >
-              <div className="relative flex h-67 w-full justify-center bg-ui">
-                <Image
-                  src={category.image || "/static/images/placeholder.webp"}
-                  alt=""
-                  width={256}
-                  height={256}
-                  className="object-contain p-4 transition-transform hover:scale-105"
-                />
-              </div>
-              <h3 className="mt-4 text-xl font-light tracking-wide">
-                {category.name}
-              </h3>
-            </Link>
-          </li>
-        ))}
+        {categories.map((category) => {
+          const url = `${process.env.NEXT_PUBLIC_IMAGE_CDN_URL}${category.image}`;
+          return (
+            <li key={category.id}>
+              <Link
+                href={`${ROUTES.CATEGORY}/${category.slug}`}
+                key={category.id}
+              >
+                <div className="relative flex h-67 w-full justify-center bg-ui">
+                  <Image
+                    src={url || "/static/images/placeholder.webp"}
+                    alt=""
+                    width={256}
+                    height={256}
+                    className="object-contain p-4 transition-transform hover:scale-105"
+                  />
+                </div>
+                <h3 className="mt-4 text-xl font-light tracking-wide">
+                  {category.name}
+                </h3>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
