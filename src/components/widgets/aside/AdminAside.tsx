d2df69a2b4c9c504ad77e636bc5e54204777e025
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-11 09:45:02 +0300

    feat(admin): add variations & variation options to sidebar
    - Refactored sidebar to use centralized ADMIN_ROUTES config

--- src/components/widgets/aside/AdminAside.tsx
+++ src/components/widgets/aside/AdminAside.tsx
@@ -1,17 +1,53 @@
 import { NavLink } from "@/components/features/links/NavLink";
+import { ADMIN_ROUTES } from "@/utils/config/routes/adminRoutes";
 // import Link from "next/link";
 
 // interface IProps {}
+const asideRoutes = [
+  {
+    name: "Products (Home)",
+    href: `${ADMIN_ROUTES.ADMIN}`,
+  },
+  {
+    name: "Product Variants",
+    href: `${ADMIN_ROUTES.PRODUCTS}`,
+  },
+  {
+    name: "Orders",
+    href: `${ADMIN_ROUTES.ORDERS}`,
+  },
+  {
+    name: "Reviews",
+    href: `${ADMIN_ROUTES.REVIEWS}`,
+  },
+  {
+    name: "Categories",
+    href: `${ADMIN_ROUTES.CATEGORIES}`,
+  },
+  {
+    name: "Variations",
+    href: `${ADMIN_ROUTES.VARIATIONS}`,
+  },
+  {
+    name: "Variations Options",
+    href: `${ADMIN_ROUTES.VARIATIONS_OPTIONS}`,
+  },
+];
 
 export const AdminAside = () => {
   return (
     <aside className="flex w-80 flex-col gap-5 border-r border-border bg-ui py-4 sm:px-2 md:px-4 lg:px-6 xl:px-8 2xl:px-10">
-      {/* <Link href="/admin">Home</Link> */}
-      <NavLink href="/admin">Products (Home)</NavLink>
-      <NavLink href="/admin/products">Product Variants</NavLink>
-      <NavLink href="/admin/orders">Orders</NavLink>
-      <NavLink href="/admin/reviews">Reviews</NavLink>
-      <NavLink href="/admin/categories">Categories</NavLink>
+      <div className="flex flex-col gap-2">
+        {asideRoutes.map((route) => (
+          <NavLink
+            className="rounded-md px-2 py-1 transition-colors hover:bg-app"
+            key={route.name}
+            href={route.href}
+          >
+            {route.name}
+          </NavLink>
+        ))}
+      </div>
     </aside>
   );
 };
