10383e18abcaf9a26892e058ece058c13f1d802a
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-11 09:45:29 +0300

    feat(admin): added new admin routes to config

--- src/utils/config/routes/adminRoutes.ts
+++ src/utils/config/routes/adminRoutes.ts
@@ -2,6 +2,9 @@ export const ADMIN_ROUTES = {
   ADMIN: "/admin",
   PRODUCTS: "/admin/products",
   PRODUCT: "/admin/product",
+  ORDERS: "/admin/orders",
   REVIEWS: "/admin/reviews",
   CATEGORIES: "/admin/categories",
+  VARIATIONS: "/admin/variations",
+  VARIATIONS_OPTIONS: "/admin/options",
 };
