5b4052f843d16f9efe7126faf0ba236820760f3a
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-22 17:00:54 +0300

    feat: noBarrelFiles & add role as img for svg icon

--- src/components/shared/overlays/LoadingOverlay.tsx
+++ src/components/shared/overlays/LoadingOverlay.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useOverlayStore } from "@/utils/hooks";
+import { useOverlayStore } from "@/utils/hooks/store/useOverlayStore";
 import { SquirrelIcon } from "lucide-react";
 
 export const LoadingOverlay = () => {
@@ -8,7 +8,11 @@ export const LoadingOverlay = () => {
   return isOverlayLoading ? (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-app/50">
       <div className="absolute top-0 left-0 h-1 w-full animate-pulse bg-primary" />
-      <SquirrelIcon size={100} className="animate-pulse text-primary" />
+      <SquirrelIcon
+        role="img"
+        size={100}
+        className="animate-pulse text-primary"
+      />
     </div>
   ) : null;
 };
