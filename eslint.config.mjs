d49eb31ac631d5e94c5b276f55c2470ab1fcacab
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-22 16:29:44 +0300

    chore(eslint): enable jest, jest-dom and testing-library recommended configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+// import pluginJest from "eslint-plugin-jest";
+// import pluginJestDom from "eslint-plugin-jest-dom";
+// import pluginTestingLibrary from "eslint-plugin-testing-library";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,39 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:jest/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
+  ),
+  // {
+  //   files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  //   plugins: {
+  //     jest: pluginJest,
+  //     "jest-dom": pluginJestDom,
+  //     "testing-library": pluginTestingLibrary,
+  //   },
+  //   rules: {
+  //     ...pluginJest.configs.recommended.rules,
+  //     ...pluginJestDom.configs.recommended.rules,
+  //     // You can override/add rules here:
+  //     "jest/expect-expect": "warn",
+  //   },
+  //   languageOptions: {
+  //     globals: {
+  //       jest: "readonly",
+  //       describe: "readonly",
+  //       it: "readonly",
+  //       test: "readonly",
+  //       expect: "readonly",
+  //       beforeEach: "readonly",
+  //       afterEach: "readonly",
+  //     },
+  //   },
+  // },
 ];
 
 export default eslintConfig;
