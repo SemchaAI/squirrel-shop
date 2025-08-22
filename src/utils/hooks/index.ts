849ad6f73828f5370e49a6df1f765927c5ea7dbd
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-22 16:37:34 +0300

    chore: noBarrelFiles

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -6,9 +6,9 @@ export { useModal } from "./useModal";
 export { useCooldown } from "./useCooldown";
 
 //store
-export { useCartStore } from "./store/useCartStore";
-export { useFavoriteStore } from "./store/useFavoriteStore";
-export { useOverlayStore } from "./store/useOverlayStore";
+// export { useCartStore } from "./store/useCartStore";
+// export { useFavoriteStore } from "./store/useFavoriteStore";
+// export { useOverlayStore } from "./store/useOverlayStore";
 
 //query
 export { useProductMutation } from "./query/useProduct";
