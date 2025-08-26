28dd95d1a669e7d120bd56628924b27727d8f9ec
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-08-26 17:30:54 +0300

    feat: add mock for basic next js components(next/image,next/link)

---
+++ jest.setup.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+jest.mock(
+  "next/image",
+  () =>
+    function Image({ src, alt }: { src: string; alt: string }) {
+      // eslint-disable-next-line @next/next/no-img-element
+      return <img src={src} alt={alt} />;
+    },
+);
+jest.mock("next/link", () => {
+  return function Link({
+    children,
+    href,
+  }: {
+    children: React.ReactNode;
+    href: string;
+  }) {
+    return <a href={href}>{children}</a>;
+  };
+});
