b5595a6a03b667ac45046b8037465e348cb1ffc0
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-10-22 17:14:05 +0300

    refactor: improved animation
    changed useAnimatedPresence to framer-motion for animation
    button doesn`t trigger hover/focus events

--- src/components/widgets/cart/MiniCart.tsx
+++ src/components/widgets/cart/MiniCart.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { MiniProductCart } from "@/components/entities/cards/MiniProductCart";
 import { Container } from "@/components/shared/containers/Container";
-import { useAnimatedPresence } from "@/utils/hooks/useAnimatedPresence";
 import { useCartStore } from "@/utils/hooks/store/useCartStore";
 import { ROUTES } from "@/utils/config/routes/routes";
 
@@ -16,21 +16,35 @@ export const MiniCart = ({ isOpen }: IProps) => {
   const totalAmount = useCartStore((state) => state.totalAmount);
   const isLoading = useCartStore((state) => state.isLoading);
 
-  const shouldRender = useAnimatedPresence(isOpen, 300);
   //top-14
   return (
-    <Container
-      className={`absolute top-10 right-0 z-10 w-110 bg-ui transition-all duration-300 ${isOpen ? "animate-fade-in-down" : "animate-fade-out-up"} ${shouldRender ? "" : "hidden"} `}
-    >
-      {items && (
-        <div className="flex flex-col">
+    <AnimatePresence>
+      <motion.div
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        transition={{ duration: 0.35 }}
+        variants={{
+          open: {
+            opacity: 1,
+            transform: "translateY(0)",
+            pointerEvents: "auto",
+          },
+          closed: {
+            opacity: 0,
+            transform: "translateY(-150%)",
+            pointerEvents: "none",
+          },
+        }}
+        className="absolute top-20 right-0 z-10 w-110"
+      >
+        <Container className="flex flex-col bg-ui">
           <h2 className="text-xl">Shopping Cart</h2>
           {items.length === 0 ? (
             <div className="flex h-40 items-center justify-center rounded-md bg-app font-bold">
               Cart is empty
             </div>
           ) : (
-            <div className="flex max-h-40 flex-col gap-4 overflow-y-auto pt-2 pr-[15px]">
+            <div className="customScrollbar flex max-h-40 flex-col gap-4 overflow-y-auto pt-2 pr-[15px]">
               {items.map((item) => (
                 <MiniProductCart
                   key={item.id}
@@ -58,15 +72,14 @@ export const MiniCart = ({ isOpen }: IProps) => {
 
               <Link
                 className="rounded-md bg-black px-4 py-3 text-white transition-colors hover:bg-primary"
-                // disabled={isLoading || items.length === 0}
                 href={ROUTES.CHECKOUT}
               >
                 Checkout
               </Link>
             </div>
           </div>
-        </div>
-      )}
-    </Container>
+        </Container>
+      </motion.div>
+    </AnimatePresence>
   );
 };
