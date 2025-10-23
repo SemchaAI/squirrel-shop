25654e748ad577ed329e848ef18fd797ad35d905
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-10-23 11:04:52 +0300

    chore: removed deprecated helper

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -2,6 +2,5 @@ export { saltAndHashPassword, comparePasswords } from "./bcrypt";
 export { fetcher } from "./fetcher";
 export { createNextResponse, createResponse } from "./createNextResponse";
 export { groupCloseVariantsByVariation } from "./groupCloseVariantsByVariation";
-export { transformAttributes } from "./transformAttributes";
 export { updateSearchParams } from "./buildUrlQuery";
 export { addMinutes, isBefore } from "./time";
