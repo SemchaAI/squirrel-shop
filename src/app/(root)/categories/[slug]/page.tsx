f0443ee3b9ea626339eecda8c7760d537d35fd33
Semciuc Nichita <76156008+SemchaAI@users.noreply.github.com>
2025-10-23 10:53:43 +0300

    refactor(category): simplify CategoryPage to use unified getCategoryProducts API

--- src/app/(root)/categories/[slug]/page.tsx
+++ src/app/(root)/categories/[slug]/page.tsx
@@ -1,13 +1,9 @@
-import { notFound } from "next/navigation";
-
 import { PromoBanner } from "@/components/widgets/banners/PromoBanner";
 import { ProductList } from "@/components/widgets/lists/ProductList";
 import { Pagination } from "@/components/features/pagination/Pagination";
 import { Filters } from "@/components/features/filters/Filters";
 
-import { getCategoryExists } from "@/utils/api/getCategoryExists";
-import { getFilteredCategory } from "@/utils/api/getFilteredCategory";
-import { getFiltersAttributes } from "@/utils/api/getFiltersAttributes";
+import { getCategoryProducts } from "@/utils/api/getCategoryPage";
 
 export default async function CategoryPage({
   params,
@@ -17,26 +13,22 @@ export default async function CategoryPage({
   searchParams: Promise<Record<string, string | string[] | undefined>>;
 }>) {
   const { slug } = await params;
-  const isCategoryExists = await getCategoryExists(slug);
-  if (!isCategoryExists) return notFound();
-
   const search = await searchParams;
-  const { data, meta } = await getFilteredCategory(slug, search);
-
-  const attributesInCategory = await getFiltersAttributes(slug);
+  const { data, meta } = await getCategoryProducts(slug, search);
 
   return (
     <div className="wrapper my-5 flex flex-col justify-start gap-8">
       <PromoBanner />
       <div className="flex flex-col gap-5 md:flex-row">
         {/* <div className="flex grow justify-end"> */}
-        <Filters
-          price={meta.priceRange}
-          attributesInCategory={attributesInCategory}
-        />
+        <Filters price={meta.priceRange} attributes={data.attributes} />
         {/* </div> */}
         <div className="flex flex-1 flex-col">
-          <ProductList priorityIndex={2} products={data} title={slug} />
+          <ProductList
+            priorityIndex={2}
+            products={data.productVariants}
+            title={slug}
+          />
           <Pagination
             currPage={meta.pageNumber}
             count={meta.total}
